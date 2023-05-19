d2df6e36fcb07988e9e35d5505a56ba2d4c8782b
InvisEA <lacitizen@seznam.cz>
2023-05-19 07:57:20 +0200

    Rewrote alien_invasion.py so settings are got from setting.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,16 +3,18 @@
 import sys
 
 import pygame
+from settings import Settings
 
 class AlienInvasion:
 	"""Class for resources and game behaviour handling."""
 	def __init__(self):
 		"""Initializes the game and creates game resources."""
 		pygame.init()
-		self.screen = pygame.display.set_mode((1200, 800))
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
-		# Setting background color.
-		self.bg_color = (230, 230, 230)
 
 	def run_game(self):
 		"""Launch main cycle of the game."""
@@ -23,7 +25,7 @@ class AlienInvasion:
 					sys.exit()
 
 			# filling the color to background in every iteration
-			self.screen.fill(self.bg_color)
+			self.screen.fill(self.settings.bg_color)
 
 			# Displaying last drawn screen.
 			pygame.display.flip()
