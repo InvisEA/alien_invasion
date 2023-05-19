d082ca27b3c33eba1347bbb700f652720986c3cb
InvisEA <lacitizen@seznam.cz>
2023-05-19 07:48:24 +0200

    Added setting.py for handling settings of the whole game

---
+++ settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+class Settings():
+	"""Class for saving all settings of Alien Invasion game."""
+
+	def __init__(self):
+		"""Initializes game settings."""
+		# Scree parameters
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
+		
