806299ddea02c1d59890f0fca1acb7864c1b6cae
InvisEA <lacitizen@seznam.cz>
2023-05-19 15:28:34 +0200

    Added class that allows creating a ship

---
+++ ship.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pygame
+
+class Ship():
+	"""Class for controlling the ship."""
+	def __init__(self, hi_game):
+		self.screen = hi_game.screen
+		self.screen_rect = hi_game.screen.get_rect()
+
+		# Loads image of the ship and gets rectangle.
+		self.image = pygame.image.load('images/spaceship.png')
+		self.rect = self.image.get_rect()
+		# New spaceship appears on the bottom border of the screen.
+		self.rect.midbottom = self.screen_rect.midbottom
+
+	def blitme(self):
+		"""Paints ship in the current position."""
+		self.screen.blit(self.image, self.rect)
