d5a99ed6c92229f36c4e3ab0308aee05e4d31f1a
InvisEA <lacitizen@seznam.cz>
2023-05-19 08:08:09 +0200

    Changed the namings

---
+++ human_invasion.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+import pygame
+from settings import Settings
+
+class HumanInvasion:
+	"""Class for resources and game behaviour handling."""
+	def __init__(self):
+		"""Initializes the game and creates game resources."""
+		pygame.init()
+		self.settings = Settings()
+
+		self.screen = pygame.display.set_mode(
+			(self.settings.screen_width, self.settings.screen_height))
+		pygame.display.set_caption("Human Invasion")
+
+	def run_game(self):
+		"""Launch main cycle of the game."""
+		while True:
+			# Monitoring events of keyboard and mouse.
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+
+			# filling the color to background in every iteration
+			self.screen.fill(self.settings.bg_color)
+
+			# Displaying last drawn screen.
+			pygame.display.flip()
+
+if __name__ == '__main__':
+	# create instance and launch the game
+	ai = HumanInvasion()
+	ai.run_game()
