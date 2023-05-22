1234488af00d3e9c11ad92d94b84de0a4788c6c7
InvisEA <lacitizen@seznam.cz>
2023-05-22 11:35:16 +0200

    Added setting regulating the speed of the ship

--- settings.py
+++ settings.py
@@ -5,8 +5,10 @@ class Settings():
 
 	def __init__(self):
 		"""Initializes game settings."""
-		# Scree parameters
+		# Screen parameters
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
+		# ship speed adjusting
+		self.ship_speed = 1.5
 		
