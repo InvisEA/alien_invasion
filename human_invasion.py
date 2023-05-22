b3e7e90784f2a73f49a5a81d986704f32a4405e1
InvisEA <lacitizen@seznam.cz>
2023-05-22 11:15:14 +0200

    Added tracking of LEFT and RIGHT arrows events. So right now the ship can move!

--- human_invasion.py
+++ human_invasion.py
@@ -23,6 +23,7 @@ class HumanInvasion:
 		"""Launch main cycle of the game."""
 		while True:
 			self._check_events()
+			self.ship.update()
 			self._update_screen()
 			
 	def _check_events(self):
@@ -30,6 +31,19 @@ class HumanInvasion:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_RIGHT:
+					# Setting a flag for moving the ship to the right.
+					self.ship.moving_right = True
+				elif event.key == pygame.K_LEFT:
+					# Setting a flag for moving the ship to the left.
+					self.ship.moving_left = True
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_RIGHT:
+					self.ship.moving_right = False
+				elif event.key == pygame.K_LEFT:
+					self.ship.moving_left = False		
+
 
 	def _update_screen(self):
 		# filling the color to background in every iteration
