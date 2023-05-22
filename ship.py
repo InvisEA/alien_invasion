486a9c8e8b8759ccd1b422bcc027d680dc15b5d8
InvisEA <lacitizen@seznam.cz>
2023-05-22 11:17:52 +0200

    Added new attributes that are flags allowing to track if corresponding arrow on the keyboard is pressed. Added new method that changes x coordinate of the ship depending on the flag value

--- ship.py
+++ ship.py
@@ -10,14 +10,28 @@ class Ship():
 
 		# Loads image of the ship and gets rectangle.
 		self.image = pygame.image.load('images/spaceship.png').convert_alpha()
+		
 		# rescale the image of the ship
 		w = self.image.get_width()
 		h = self.image.get_height()
 		self.image = pygame.transform.scale(self.image, (w * 0.15, h * 0.15))
 		self.rect = self.image.get_rect()
+
 		# New spaceship appears on the bottom border of the screen.
 		self.rect.midbottom = self.screen_rect.midbottom
+
+		# flag that tracks moving
+		self.moving_right = False
+		self.moving_left = False
+
 
 	def blitme(self):
 		"""Paints ship in the current position."""
 		self.screen.blit(self.image, self.rect)
+
+	def update(self):
+		"""Updates the position of the ship with respect to certain flag."""
+		if self.moving_right:
+			self.rect.x += 1
+		if self.moving_left:
+			self.rect.x -= 1
