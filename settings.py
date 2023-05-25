829669a06e779cbfa4b28f93957faddfc7f7a841
InvisEA <lacitizen@seznam.cz>
2023-05-25 07:50:17 +0200

    Changed settings.py so it contains settings for bullet

--- settings.py
+++ settings.py
@@ -9,6 +9,12 @@ class Settings():
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
+		
 		# ship speed adjusting
 		self.ship_speed = 1.5
 		
+		# bullet settings
+		self.bullet_speed = 1
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 160, 60)
