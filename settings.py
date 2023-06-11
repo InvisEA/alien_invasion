ff11ccc31a9f76e79f66cd6000e27ee58f733d21
InvisEA <lacitizen@seznam.cz>
2023-06-11 07:59:59 +0200

    Added new setting

--- settings.py
+++ settings.py
@@ -12,9 +12,10 @@ class Settings():
 		
 		# ship speed adjusting
 		self.ship_speed = 1.5
+		self.ship_limit = 3
 		
 		# bullet settings
-		self.bullet_speed = 1
+		self.bullet_speed = 1.5
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 160, 60)
