ec7bb2fb04ab63d7a6e6e4479511d5d4e2f2ecf9
InvisEA <lacitizen@seznam.cz>
2023-06-14 07:28:47 +0200

    Groupped dynamic setting into separate function that is used to initialize them and reset when player starts a new game

--- settings.py
+++ settings.py
@@ -11,18 +11,33 @@ class Settings():
 		self.bg_color = (230, 230, 230)
 		
 		# ship speed adjusting
-		self.ship_speed = 1.5
 		self.ship_limit = 2
 		
 		# bullet settings
-		self.bullet_speed = 1.5
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60, 160, 60)
 		self.bullets_allowed = 5
 
 		# invaders settings
-		self.invader_speed = 1.0
 		self.fleet_drop_speed = 10
-		# fleet_direction = 1 means moving to the right, -1 - to the left
+
+		# factor of increasing tempo of the game
+		self.speedup_scale = 1.2
+
+		self.initialize_dynamic_settings()
+
+
+	def initialize_dynamic_settings(self):
+		"""Initializes settings changing during the game."""
+		self.ship_speed = 1.5
+		self.bullet_speed = 3
+		self.invader_speed = 1.0
+
+		# fleet_direction = 1 if moving to the right; -1 - to the left
 		self.fleet_direction = 1
+
+	def increase_speed(self):
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.invader_speed *= self.speedup_scale
